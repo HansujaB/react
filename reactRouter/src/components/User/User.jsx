7b56088ed858ab8aaed6bec687d6666b8b5ff499
HansujaB <hansujaigdtuwcseai@gmail.com>
2025-08-13 02:16:53 +0530

    fixing bugs-router

--- reactRouter/src/components/User/User.jsx
+++ reactRouter/src/components/User/User.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 function User() {
-    const {userId} =useParams()
+    const {userid} =useParams()
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-     User : {userId}
+     User : {userid}
     </div>
   )
 }
